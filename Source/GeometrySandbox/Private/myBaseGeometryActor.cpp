0d1d745aa4bc5ec7061c0b0914887ae05087ed78
cgdim <supertema@gmail.com>
2022-10-16 15:01:50 +0400

    000

--- Source/GeometrySandbox/Private/myBaseGeometryActor.cpp
+++ Source/GeometrySandbox/Private/myBaseGeometryActor.cpp
@@ -2,6 +2,7 @@
 
 
 #include "myBaseGeometryActor.h"
+#include "Engine/Engine.h"
 
 DEFINE_LOG_CATEGORY_STATIC(aLogBaseGeometry, All, All)
 
@@ -18,44 +19,41 @@ void AmyBaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	//PrintTypes();
-
-	FString Name = "John Connor";
-	UE_LOG(aLogBaseGeometry, Display, TEXT("Name: %s"), *Name);
-
-	int WeaponsNum = 4;
-	float Health = 34.874555f;
-	bool IsDead = false;
+	PrintTypes();
 	
-	FString WeaponsNumStr = "Weapons num = " + FString::FromInt(WeaponsNum);
-	FString HealthStr = "Health = " + FString::SanitizeFloat(Health);
-	FString IsDeadStr = "Is Dead = " + FString(IsDead ? "True" : "False");
-
-	FString Stat = FString::Printf(TEXT(" \n == ALL STAT ==  \n %s \n %s \n %s "), *WeaponsNumStr, *HealthStr, *IsDeadStr);
-	UE_LOG(aLogBaseGeometry, Warning, TEXT("Stat"));
+	// PrintStringTypes();
 
 	
 }
 
 void AmyBaseGeometryActor::PrintTypes()
 {
-	UE_LOG(aLogBaseGeometry,Display,TEXT("Привет. Как дела?"));
-	UE_LOG(aLogBaseGeometry,Warning,TEXT("Привет. Как дела?"));
-	UE_LOG(aLogBaseGeometry,Error,TEXT("Привет. Как дела?"));
-
-	int WeaponsNum = 4;
-	int KillsNum = 7;
-	float Health = 34.874555f;
-	bool IsDead = false;
-	bool HasWeapon = true;
-
-	UE_LOG(aLogBaseGeometry,Warning,TEXT("Weapons num: %d, kills num %i"), WeaponsNum, KillsNum);
-	UE_LOG(aLogBaseGeometry,Warning,TEXT("Health: %.f"), Health);
-	UE_LOG(aLogBaseGeometry,Warning,TEXT("Health: %.2f"), Health);
-	UE_LOG(aLogBaseGeometry,Warning,TEXT("Is Dead: %i"), IsDead);
-	UE_LOG(aLogBaseGeometry,Warning,TEXT("Is Dead: %d"), IsDead);
-	UE_LOG(aLogBaseGeometry,Warning,TEXT("Has Weapon: %d"), static_cast<int>(HasWeapon));
+	UE_LOG(aLogBaseGeometry,Warning,TEXT("Actor Name: %s"), *GetName());
+	UE_LOG(aLogBaseGeometry,Display,TEXT("Weapons num: %d, kills num %i"), WeaponsNum, KillsNum);
+	UE_LOG(aLogBaseGeometry,Display,TEXT("Health: %.f"), Health);
+	UE_LOG(aLogBaseGeometry,Display,TEXT("Is Dead: %d"), IsDead);
+	UE_LOG(aLogBaseGeometry,Display,TEXT("Has Weapon: %d"), static_cast<int>(HasWeapon));
 }
+
+void AmyBaseGeometryActor::PrintStringTypes()
+{
+	UE_LOG(aLogBaseGeometry,Warning,TEXT("Actor Name: %s"), *GetName());
+
+	FString Name = "John Connor";
+	UE_LOG(aLogBaseGeometry, Display, TEXT("Name: %s"), *Name);
+	
+	FString WeaponsNumStr = "Weapons num = " + FString::FromInt(WeaponsNum);
+	FString HealthStr = "Health = " + FString::SanitizeFloat(Health);
+	FString IsDeadStr = "Is Dead = " + FString(IsDead ? "True" : "False");
+
+	FString Stat = FString::Printf(TEXT(" \n == ALL STAT ==  \n %s \n %s \n %s "), *WeaponsNumStr, *HealthStr, *IsDeadStr);
+	UE_LOG(aLogBaseGeometry, Warning, TEXT("%s"), *Stat);
+	
+	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Blue, Name, true);
+	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Green, Stat, true);
+	
+}
+
 
 // Called every frame
 void AmyBaseGeometryActor::Tick(float DeltaTime)
--- Source/GeometrySandbox/Public/myBaseGeometryActor.h
+++ Source/GeometrySandbox/Public/myBaseGeometryActor.h
@@ -19,8 +19,24 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, Category="Properties")
+	int32 WeaponsNum = 4;
+
+	UPROPERTY(EditAnywhere, Category="Properties")
+	int32 KillsNum = 7;
+
+	UPROPERTY(EditAnywhere, Category="Properties")
+	float Health = 34.874555f;
+
+	UPROPERTY(EditAnywhere, Category="Properties")
+	bool IsDead = false;
+
+	UPROPERTY(EditAnywhere, Category="Properties")
+	bool HasWeapon = true;
+
 private:
 	void PrintTypes();
+	void PrintStringTypes();
 
 public:	
 	// Called every frame
